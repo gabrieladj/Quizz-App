2bf0eee8e096a120f8256e4c7fd765d24cc3425d
NIKITPATEL <niks242djpatel@gmail.com>
2023-09-17 19:24:26 -0400

    ldnflsnf

--- src/app/HomePage.js
+++ src/app/HomePage.js
@@ -3,9 +3,15 @@ import React from "react";
 const HomePage = () =>{
     return (
         <main>
-            <div >
-            <h1 className="text-center m-10 p-10 font-semibold "> Parctise the Quizz and get smarter</h1>
-            <p>Please Login</p>
+            <div className="m-10 p-10" >
+            <h1 className="text-center font-semibold "> Parctise the Quizz and get smarter</h1>
+            <p>Assessment, instruction, and practise htat motivate every student</p>
+            <h2>Helloe  Nikt</h2>
+            </div>
+
+            <div className="buttons-container inline-grid gap-2 grid-cols-2">
+              <a href="/">Teachers</a>
+              <a href="/">Students</a>
             </div>
 
         </main>
