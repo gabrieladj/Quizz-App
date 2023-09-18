db560ff473e7eb62673bf5806c934bd13f40fbe4
NIKITPATEL <niks242djpatel@gmail.com>
2023-09-17 20:01:26 -0400

    sefs

--- src/app/HomePage.js
+++ src/app/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () =>{
             <div className="m-10 p-10" >
             <h1 className="text-center font-semibold "> Parctise the Quizz and get smarter</h1>
             <p>Assessment, instruction, and practise htat motivate every student</p>
-            <h2>Helloe  Nikt</h2>
+            <h2>Hellow  Nikt</h2>
             </div>
 
             <div className="buttons-container inline-grid gap-2 grid-cols-2">
