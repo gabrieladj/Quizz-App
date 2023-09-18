cc289e06b7be1bce206cb7d7db928b45d7713b99
NIKITPATEL <niks242djpatel@gmail.com>
2023-09-18 11:34:32 -0400

    First Change

--- src/app/HomePage.js
+++ src/app/HomePage.js
@@ -5,13 +5,22 @@ const HomePage = () =>{
         <main>
             <div className="m-10 p-10" >
             <h1 className="text-center font-semibold "> Parctise the Quizz and get smarter</h1>
-            <p>Assessment, instruction, and practise htat motivate every student</p>
-            <h2>Hellow  Nikt</h2>
+            <p className="text-center">Assessment, instruction, and practise htat motivate every student</p>
+            
             </div>
 
-            <div className="buttons-container inline-grid gap-2 grid-cols-2">
-              <a href="/">Teachers</a>
-              <a href="/">Students</a>
+
+            <div className="buttons-container flex justify-center">
+                <div className="inline-grid gap-2 grid-cols-2">
+                    <div>
+                        <button href="/" className="rounded-lg bg-blue-300 px-6 py-3 text-left">Teacher<p>Sign Up for free</p></button>
+                        
+                    </div>
+                    <div>
+                        <button href="/" className="rounded-lg bg-gray-300 px-6 py-3 text-left">Student<p>Login as student</p></button>
+                        
+                    </div>
+                </div>
             </div>
 
         </main>
