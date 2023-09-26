dc249dc5b2741e4363897852e0a99bc0d3f9e9a0
gabriela <143661476+gabrieladj@users.noreply.github.com>
2023-09-25 20:37:49 -0400

    Create question.test.js

---
+++ __tests__/question.test.js
@@ -0,0 +1,19 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+afterAll(async () => {
+  await prisma.$disconnect();
+});
+
+describe('Question Model', () => {
+  it('should create a question', async () => {
+    const Question = await prisma.question.create({
+      data: {
+        content: 'Is this a test question?',
+      },
+    });
+
+    expect(Question.content).toBe('Is this a test question?');
+  });
+
+});
