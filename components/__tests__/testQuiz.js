7b516602272765de96b4f0052390d191ba47d317
gabriela <143661476+gabrieladj@users.noreply.github.com>
2023-09-25 20:50:11 -0400

    Create testQuiz.js

---
+++ components/__tests__/testQuiz.js
@@ -0,0 +1,11 @@
+// pages/__tests__/quizPage.test.js
+import { render, screen } from '@testing-library/react';
+import QuizPage from '../quiz/page'; // Import our component
+
+test('renders quiz page', () => {
+  render(<QuizPage />);
+
+  // Use queries from @testing-library/react to assert on your component's behavior
+  const heading = screen.getByText(/Quiz/);
+  expect(heading).toBeInTheDocument();
+});
