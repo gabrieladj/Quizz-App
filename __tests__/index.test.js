435e1bef6807a0278d09ed07d16de2ab8c9a0667
ajh1043 <ajh1043@plymouth.edu>
2023-10-05 20:03:40 -0400

    testing finally works

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import Home from '@/pages/index';
 import '@testing-library/jest-dom';
+import { useRouter } from 'next/router'
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn()
+}))
  
 describe('Home', () => {
+  //useRouter.mockReturnValue({ query: {}})
   it('renders a heading', () => {
     render(<Home />)
- 
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
-    })
- 
-    expect(heading).toBeInTheDocument()
+
+    // const heading = screen.getByRole('heading', {
+    //   name: /welcome to next\.js!/i,
+    // })
+
+    expect(screen.getByText("Practise the Quizz and get smarter")).toBeInTheDocument()
   });
 });
